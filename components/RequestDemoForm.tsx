e5dbeb03403442912a87b970e61445d92f3b7508
kingsley achumie <kingsonly13c@gmail.com>
2025-05-19 14:02:37 +0100

    wip

--- components/RequestDemoForm.tsx
+++ components/RequestDemoForm.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { CheckCircle } from "lucide-react";
+import Image from "next/image"
 import { Button } from "@/components/ui/button";
 import {
   RequestDemoFormInput,
@@ -209,11 +210,11 @@ export default function RequestDemoForm() {
                 </div>
 
                 <Button
-                  className="w-full bg-blue-600 hover:bg-blue-700 text-white py-4 text-lg"
-                  type="submit"
-                  disabled={isSubmitting}
+                  className={`w-full ${isSubmitting ? "bg-white" : "bg-blue-600"} ${isSubmitting ? "hover:bg-white" : "hover:bg-blue-700"}  text-white py-4 text-lg`}
+                  type={isSubmitting ? "button" : "submit"}
+
                 >
-                  {isSubmitting ? "Submitting..." : "Request Demo"}
+                  {isSubmitting ? <Image alt="loader" src="/images/loader.gif" width={100} height={100} /> : "Request Demo"}
                 </Button>
 
                 <p className="text-sm text-gray-500 text-center">
